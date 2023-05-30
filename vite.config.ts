4749935af2abfed28e458b9ac18ba9a3bfe97c3a
Asandei Stefan <asandei.stefanel@gmail.com>
2023-05-30 11:29:27 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { copyFileSync } from "fs";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import * as vite from "vite";
+import { PluginOptions } from "vite-plugin-dts";
+
+const copyToExample = (options?: PluginOptions): vite.Plugin => {
+  return {
+    name: "copyToExample",
+    closeBundle: () => {
+      const build = "./dist/main.js";
+      const example = "./example/wasm-runner.js";
+      copyFileSync(build, example);
+      console.log(`copied bundle to example`);
+    },
+  };
+};
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "Wasm Runner",
+      fileName: "main",
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {},
+      },
+    },
+  },
+  plugins: [copyToExample()],
+});
